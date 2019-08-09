c5ee4e92f91e06a3cf1090e50aa943ba62905dfb
qyx1025369045 <1025369045@qq.com>
2019-08-09 09:55:08 +0800

    动画

--- app/src/main/java/com/example/qiaoyanxiong/resenter/activity/MainActivity.java
+++ app/src/main/java/com/example/qiaoyanxiong/resenter/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.qiaoyanxiong.resenter;
+package com.example.qiaoyanxiong.resenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.qiaoyanxiong.resenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
