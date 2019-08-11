fcb10f3a1efa38d9cb850bf2af6eee842f3d0117
qyx1025369045 <1025369045@qq.com>
2019-08-11 09:14:24 +0800

    hyy

--- app/src/main/java/com/example/qiaoyanxiong/resenter/app/MyApp.java
+++ app/src/main/java/com/example/qiaoyanxiong/resenter/app/MyApp.java
@@ -1,7 +1,7 @@
 package com.example.qiaoyanxiong.resenter.app;
 
 import android.app.Application;
-<<<<<<< HEAD
+
 import android.content.Context;
 
 public class MyApp extends Application {
@@ -15,23 +15,19 @@ public class MyApp extends Application {
     public static Context getContext() {
         return null;
     }
-=======
+
 
 public class MyApp extends Application {
 
     private static MyApp app;
->>>>>>> d5f3668f65337b26bcd60a53cdf64cc9e6a469ae
 
     @Override
     public void onCreate() {
         super.onCreate();
-<<<<<<< HEAD
-=======
         app = this;
     }
 
     public static MyApp getInstance() {
         return app;
->>>>>>> d5f3668f65337b26bcd60a53cdf64cc9e6a469ae
     }
 }
