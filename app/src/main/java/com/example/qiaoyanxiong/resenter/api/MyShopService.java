ee213cf53f94178a781c44c14b9f407bf4db8046
ninongsha123 <2452140264@qq.com>
2019-08-12 09:25:27 +0800

    ppp

--- app/src/main/java/com/example/qiaoyanxiong/resenter/api/MyShopService.java
+++ app/src/main/java/com/example/qiaoyanxiong/resenter/api/MyShopService.java
@@ -1,9 +1,6 @@
 package com.example.qiaoyanxiong.resenter.api;
 
-<<<<<<< HEAD
 import com.example.qiaoyanxiong.resenter.homebean.HomeBean;
-=======
->>>>>>> d5f3668f65337b26bcd60a53cdf64cc9e6a469ae
 import com.example.qiaoyanxiong.resenter.shop.bean.BookDataBean;
 import com.example.qiaoyanxiong.resenter.shop.bean.KeChengDataBean;
 import com.example.qiaoyanxiong.resenter.shop.bean.KechengBean;
@@ -12,12 +9,9 @@ import java.net.URL;
 import java.util.List;
 
 import io.reactivex.Observable;
-<<<<<<< HEAD
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
-=======
->>>>>>> d5f3668f65337b26bcd60a53cdf64cc9e6a469ae
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
@@ -25,7 +19,6 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface MyShopService {
-<<<<<<< HEAD
 
     public String url = "https://app.yiyanmeng.com/index.php/";
 
@@ -39,12 +32,9 @@ public interface MyShopService {
     //https://app.yiyanmeng.com/index.php/shop/get_shop_list?p=1
     public String shurl = "https://app.yiyanmeng.com/";
 
-=======
     //    URL https://app.yiyanmeng.com/index.php/kecheng/ke_index_list
     //https://app.yiyanmeng.com/index.php/shop/get_shop_list?p=1
-    public String url = "https://app.yiyanmeng.com/";
 
->>>>>>> d5f3668f65337b26bcd60a53cdf64cc9e6a469ae
     @POST("index.php/kecheng/ke_index_list")
     @FormUrlEncoded
     Observable<KeChengDataBean> getShop(@Field("type") int type, @Field("p") int p);
